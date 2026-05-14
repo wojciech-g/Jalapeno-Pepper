ef4bebc5fe897910f8948117cc8d2dfe9170f892
Wojciech <wojciechglogowski@icloud.com>
2026-05-14 11:08:32 +0200

    Update namespace URL in jalapeno.user.js

--- jalapeno.user.js
+++ jalapeno.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Jalapeño - PepperModPL add-on
-// @namespace    http://tampermonkey.net/
+// @namespace    https://raw.githubusercontent.com/wojciech-g/Jalapeno-Pepper/main/jalapeno.user.js
 // @version      4.6.2
 // @description  Baza Fake Promo + Przelicznik + Historia + Auto Kategorie + Light/Dark Mode + PL/EN + Poprawki moderacyjne
 // @author       Xcited (https://www.pepper.pl/profile/Xcited)
